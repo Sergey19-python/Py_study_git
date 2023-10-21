441067f46580614a94faf375598cf462f549fe91
Sergey_Konovalenko <sergey.konovalenko.2017@mail.ru>
2023-10-21 03:59:01 +0300

    konec pervogo zadania

--- main.py
+++ main.py
@@ -91,8 +91,69 @@
 # ostatok_bon = bon - (bon_one_chield * chield)
 # print(f'{bon_one_chield}\n{ostatok_bon}')
 
-# Четырнадцатое задание
-n_child = int(input())
-total = int(input())
-print(total // n_child)
-print(total % n_child)
+# # Четырнадцатое задание
+# n_child = int(input())
+# total = int(input())
+# print(total // n_child)
+# print(total % n_child)
+
+# # Пятнадцатое задание
+# chasy = int(input())
+# minute = int(input())
+# minute_dostavka = int(input())
+# minuteostatka = ((chasy * 60) + minute + minute_dostavka) % 60
+# chasyostatka = (((chasy * 60) + minute + minute_dostavka) // 60) % 24
+# print(f'{chasyostatka:02}:{minuteostatka:02}') # Можно вывести количество разрядов
+
+# Шестнадцатое задание
+# punkt_a = int(input())
+# punkt_b = int(input())
+# speed = int(input())
+# time = (punkt_b - punkt_a) / speed
+# print(f'{time:.2f}')
+
+# Семнадцатое задание
+# desyt = int(input())
+# doubl = input()
+# perevod = int(doubl, 2)
+# print(perevod + desyt)
+
+# Восемнадцатое задание
+# cena = input()
+# many = int(input())
+# perevod = int(cena, 2)
+# sdacha = many - perevod
+# print(sdacha)
+
+# Девятнадцатое задание
+# tovar = input()
+# cena = int(input())
+# ves = int(input())
+# many = int(input())
+# itog = cena * ves
+# sdacha = many - itog
+# print(f"{'Чек':=^35}")
+# print('Товар:', f"{tovar}".rjust(35-7))
+# print('Цена:', f"{ves}кг * {cena}руб/кг".rjust(35-6))
+# print('Итого:', f"{itog}руб".rjust(35-7))
+# print('Внесено:', f"{many}руб".rjust(35-9))
+# print('Сдача:', f"{sdacha}руб".rjust(35-7))
+# print('=' * 35)
+
+# Двадцатое задание
+# Формула:
+# 32 кг общий вес
+# 285 общая цена за кг
+# 300 цена за кг первого вида
+# 240 цена за кг второго вида
+# кг каждого вида - ?
+# (32 * 285 - 32 * 240) / (300 - 240) от общей прибыли отнимаем прибыль за первый вид котлет и делим
+# на разницу между стоимостью видов котлет, потом отнимаем от общей массы найденный вес первого вида и находим кг второго вида
+
+ves = int(input())
+cena_za_kg = int(input())
+cena_first = int(input())
+cena_second = int(input())
+ves_second = int((ves * cena_za_kg - ves * cena_second) / (cena_first - cena_second))
+ves_first = ves - ves_second
+print(ves_second, ves_first, sep=' ')
