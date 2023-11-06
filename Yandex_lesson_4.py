cfcf27365329a73d7bbf7e85bdf889b8985ec5b5
Sergey_Konovalenko <sergey.konovalenko.2017@mail.ru>
2023-11-06 03:01:17 +0300

    promezytochnyi commit

--- Yandex_lesson_4.py
+++ Yandex_lesson_4.py
@@ -50,22 +50,155 @@
 # print(count)
 
 # Задание 6
-number = int(input())
-a = int(input())
-nod = 0
-i = 1
-while i < number:
-    b = int(input())
-    while nod == 0:
-        if min(a, b) == 1:
-            nod = 1
-        elif min(a, b) == 0:
-            nod = max(a, b)
-        elif a > b:
-            a = a % b
-        elif b > a:
-            b = b % a
-    a = nod
-    nod = 0
-    i += 1
-print(a)
+# number = int(input())
+# a = int(input())
+# nod = 0
+# i = 1
+# while i < number:
+#     b = int(input())
+#     while nod == 0:
+#         if min(a, b) == 1:
+#             nod = 1
+#         elif min(a, b) == 0:
+#             nod = max(a, b)
+#         elif a > b:
+#             a = a % b
+#         elif b > a:
+#             b = b % a
+#     a = nod
+#     nod = 0
+#     i += 1
+# print(a)
+
+# Задание 7
+# number = int(input())
+# sec = 3
+# i = 0
+# for speeder in range(1, number + 1):
+#     while sec != 0:
+#         print(f"До старта {sec} секунд(ы)")
+#         sec -= 1
+#     print(f"Старт {speeder}!!!")
+#     i += 1
+#     sec = 3 + i
+    
+# Задание 8
+# count = int(input())
+# winner = ""
+# sum_win = 0
+# for i in range(count):
+#     name = input()
+#     number = int(input())
+#     summ = 0
+#     while number != 0:
+#         summ += number % 10
+#         number //= 10
+#     if sum_win <= summ:
+#         sum_win = summ
+#         winner = name
+# print(winner)
+
+# Задание 9
+# count = int(input())
+# otvet = ""
+# for i in range(count):
+#     number = int(input())
+#     max_number = 0
+#     while number != 0:
+#         chislo = number % 10
+#         if chislo > max_number:
+#             max_number = chislo
+#         number //= 10
+#     otvet += str(max_number)
+# print(otvet)
+
+# Задание 11
+# count_chisel = int(input())
+# count = 0
+
+# for i in range(count_chisel):
+#     number = int(input())
+#     if number == 1:
+#         continue
+#     if number == 2 or number == 3:
+#         count += 1
+#         continue
+#     k = 2
+#     flag = False
+#     while k ** 2 <= number:
+#         if number % k == 0:
+#             flag = True
+#         k += 1
+#     if flag is True:
+#         continue
+#     else:
+#         count += 1
+# print(count)
+
+# Задание 12
+# height = int(input())
+# width = int(input())
+# number = height * width
+# k = 1
+# for i in range(height):
+#     if i > 0:
+#         print()
+#     for j in range(width):
+#         if number < 10:
+#             print('{:}'.format(k), end=" ")
+#         elif number > 9 and number < 100:
+#             print('{:2}'.format(k), end=" ")
+#         else:
+#             print('{:3}'.format(k), end=" ")
+#         k += 1
+        
+# Задание 13
+# N = int(input())
+# M = int(input())
+# chislo = N * M
+
+# k = 1
+# for i in range(1, N + 1):
+#     if i > 1:
+#         print()
+#     for j in range(M):
+#         if chislo < 10:
+#             print('{:1}'.format(k), end=" ")
+#         elif chislo > 9 and chislo < 100:
+#             print('{:2}'.format(k), end=" ")
+#         else:
+#             print('{:3}'.format(k), end=" ")
+#         k += N
+#     k = i + 1
+
+# Задание 14
+# N = int(input())
+# M = int(input())
+# number = N * M
+# k = 1
+# for i in range(1, N + 1):
+#     if i > 1:
+#         print()
+#     if i % 2 != 0:
+#         for j in range(M):
+#             if number < 10:
+#                 print("{:1}".format(k), end=" ")
+#             elif number > 9 and number < 100:
+#                 print("{:2}".format(k), end=" ")
+#             else:
+#                 print("{:3}".format(k), end=" ")
+#             k += 1
+#         k -= 1
+#     else:
+#         k += M
+#         for a in range(M):
+#             if number < 10:
+#                 print("{:1}".format(k), end=" ")
+#             elif number > 9 and number < 100:
+#                 print("{:2}".format(k), end=" ")
+#             else:
+#                 print("{:3}".format(k), end=" ")
+#             k -= 1
+#         k = k + M + 1
+
+# Задание 15
